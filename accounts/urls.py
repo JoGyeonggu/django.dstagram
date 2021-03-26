0b066da02175110b7189e262eda062739586da73
JoGyeonggu <wh941104@gmail.com>
2021-03-26 13:43:22 +0900

    accounts register update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_view
-
+from .views import *
 
 # http://127.0.0.1:8000/accounts/profile/
 # 1. 프로필 페이지를 만들기
@@ -9,4 +9,5 @@ from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('login/', auth_view.LoginView.as_view(), name="login"),
     path('logout/', auth_view.LogoutView.as_view(template_name='registration/logout.html'), name="logout"),
+    path('register/', register, name='register'),
 ]
